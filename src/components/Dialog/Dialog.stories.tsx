448a4efa7688d8507bfe102f48f8e685066c897c
Ilyich Vismara <81801314+ilyichv@users.noreply.github.com>
2023-06-21 14:24:00 +0200

    fix: adjust dialog overflow (#15)

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -40,7 +40,31 @@ export const Default = (args: IDialogProps) => {
   return (
     <Dialog {...args} open={open} onClose={handleClose}>
       <div className="mb-4">
-        <p>Modal body!!</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+          ultrices mauris malesuada, aliquet nisl ornare, pellentesque risus.
+          Donec ut orci tempus, tincidunt purus sit amet, egestas magna. Duis
+          quis augue semper, varius eros vitae, aliquam urna. Vestibulum tortor
+          leo, porttitor vitae magna non, ultrices porttitor odio. Praesent
+          lorem enim, cursus vitae magna ut, accumsan aliquam purus. Suspendisse
+          ligula justo, placerat sit amet iaculis eu, hendrerit ac mauris. Etiam
+          ultrices, turpis sit amet vestibulum porttitor, velit sem facilisis
+          dui, id semper augue purus non neque. Curabitur commodo nibh sed
+          viverra commodo. Sed nec ligula sapien. In hac habitasse platea
+          dictumst. Maecenas sed ornare ex, in ultricies dui. Praesent vitae dui
+          at leo volutpat pulvinar. Duis lacinia magna et dictum facilisis.
+          Phasellus sit amet sollicitudin erat. Nulla efficitur ullamcorper urna
+          et tempus. Vivamus nec diam quis mi vehicula vestibulum. Praesent in
+          dolor lorem. Donec sit amet orci mattis, malesuada est accumsan,
+          imperdiet diam. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Maecenas porttitor turpis ac mauris ultrices maximus. Fusce eget
+          dignissim dolor, non laoreet orci. Nunc hendrerit venenatis rutrum.
+          Morbi lacinia consequat nisi, vitae lobortis orci porta quis. Quisque
+          tincidunt dictum nunc, a dapibus eros vehicula nec. Donec finibus urna
+          ut placerat finibus. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Vestibulum congue sapien nisl, quis cursus turpis
+          aliquam at.
+        </p>
       </div>
       <Button fullWidth color="primary" onClick={() => null}>
         Action
