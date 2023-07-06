97f04d2137093dd0f72ffbc8279c51895fad9269
Ilyich Vismara <ilyich@arkemis.it>
2023-07-06 17:42:53 +0200

    chore: add changeset

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,6 +3,7 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@arkejs/ui"],
+  output: "standalone",
 };
 
 module.exports = withContentlayer(nextConfig);
