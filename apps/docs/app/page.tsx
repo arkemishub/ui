75ed3b3359251be26c14e6d08bcf4d2ca2db4e90
Manolo Battista <manolo@arkemis.it>
2023-07-12 09:58:22 +0200

    test: update tests and add redirect on docs homepage

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
 export default function Home() {
+  // Temporary redirect for WIP Homepage
+  redirect("/docs/install");
   return (
     <div>
       <h1 className="mb-8 text-center text-3xl font-bold">Arke UI</h1>
