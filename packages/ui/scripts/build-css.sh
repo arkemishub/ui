43e63243d74ff5bebe4cf4b6eb7f7ca2576a1959
Ilyich Vismara <ilyich@arkemis.it>
2023-07-21 21:40:00 +0200

    fix: use css @import

--- packages/ui/scripts/build-css.sh
+++ packages/ui/scripts/build-css.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
-# Compile PostCSS files
-compile_postcss() {
-    pnpm postcss --config "$1" "$2" --base src --dir temp
-    cat temp/"$3"/**/*.css > "temp/$3.css"
-}
-
-# Remove comments from CSS files
-remove_comments() {
-    sh scripts/remove-comments.sh "$1"
-}
-
-# Transform CSS files using prejss-cli
-transform_css() {
-    prejss-cli "$1" --format commonjs --out-dir dist
+# Compile PostCSS files and transform them using prejss-cli
+build_css() {
+    pnpm postcss --config src/components "src/plugin/$1.css" --dir temp
+    prejss-cli "temp/$1.css" --format commonjs --out-dir dist
 }
 
 # Components
-components_config="src/components"
-components_src="src/components/**/*.css"
-components_out="components"
-
-compile_postcss "$components_config" "$components_src" "$components_out"
-transform_css "temp/$components_out.css"
+build_css "components"
 
 # Utilities
-utilities_config="src/utilities"
-utilities_src="src/utilities/*.css"
-utilities_out="utilities"
-
-compile_postcss "$utilities_config" "$utilities_src" "$utilities_out"
-transform_css "temp/$utilities_out.css"
-
-# Cleanup
-rm -rf temp
+build_css "utilities"
